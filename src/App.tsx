3270835cb93cacd3ad592915945e081b98b2f3e9
bada3670 <iykl3670@gmail.com>
2022-08-24 10:48:57 +0900

    🎨 dispatch 관련 코드 개선

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import AppRouter from './router/AppRouter';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { cartActions } from './store/cart';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from './store';
+import { useAppDispatch } from './store';
 import { fetchProducts } from './store/product';
 
 function App() {
-  const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useAppDispatch();
   dispatch(fetchProducts());
 
   return (
--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productStore from './store/product';
 import cartSlice from './store/cart';
+import { useDispatch } from 'react-redux';
 
 const store = configureStore({
   reducer: {
@@ -11,3 +12,4 @@ const store = configureStore({
 
 export default store;
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
